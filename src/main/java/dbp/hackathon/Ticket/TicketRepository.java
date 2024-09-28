c62e4c1617aad1b85a8d00a148c9bbcd4afd22d5
hanksvi <hanks.vargas@utec.edu.pe>
2024-09-28 16:37:16 -0500

    Integracion qr y endopoint de recaudacion de fondos listo

--- src/main/java/dbp/hackathon/Ticket/TicketRepository.java
+++ src/main/java/dbp/hackathon/Ticket/TicketRepository.java
@@ -3,7 +3,11 @@ package dbp.hackathon.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     Iterable<Ticket> findByEstudianteId(Long estudianteId);
+    List<Ticket> findByFuncionId(Long funcionId);
+
 }
